1825c17b6c4b26919ed85c1d33b5c471bd526eb8
Ygor Henrique <ygorlima.dev06@gmail.com>
2025-09-22 10:21:30 -0300

    Create ComponenteClassePrimeiro.jsx

---
+++ App/src/components/ComponenteClassePrimeiro.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class ComponenteClassePrimeiro extends React.Component {
+    
+    render(){
+        return(
+            <>
+                <h1>Componente de classe</h1>
+            </>
+        )
+    }
+}
+
+export default ComponenteClassePrimeiro
